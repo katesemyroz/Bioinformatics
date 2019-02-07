4cf65af53c7acc6e43537c6b3b4219ab45d20754
Kateryna Semyroz <kateryna.semyroz@gmail.com>
2019-02-07 01:16:42 +0100

    add docstrings

--- Assessing Assembly Quality with N50 and N75.py
+++ Assessing Assembly Quality with N50 and N75.py
@@ -1,3 +1,17 @@
+"""
+Problem: Given a collection of DNA strings representing contigs,
+we use the N statistic NXX (where XX ranges from 01 to 99)
+to represent the maximum positive integer L such that the total
+number of nucleotides of all contigs having length ≥L is at least
+XX% of the sum of contig lengths. The most commonly used such
+statistic is N50, although N75 is also worth mentioning.
+
+Given: A collection of at most 1000 DNA strings (whose combined
+length does not exceed 50 kbp).
+
+Return: N50 and N75 for this collection of strings.
+"""
+
 def get_data_from_file(filename):
     file = open(filename, "r")
     data = [len(line.replace("\n", "")) for line in file]
@@ -5,15 +19,17 @@ def get_data_from_file(filename):
     return data
 
 
-def get_nx_value(data, n):
+def get_nx_value(data, procents):
     result = 0
-    max_sum = sum(data)*n/100
+    max_sum = sum(data)*procents/100
     data.sort(reverse=True)
     for i in range(len(data)):
         result += data[i]
         if result >= max_sum:
             return data[i]
 
+
+"""Example how to use the functions is located below."""
 
 data_from_file = get_data_from_file("C:\\Users\\pc\\Downloads\\rosalind_asmq (1).txt")
 N50 = get_nx_value(data_from_file, 50)
